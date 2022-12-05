0ade06d5a720d01bbd6edc89711a4c1032685af6
Mitch Diamond <mitch.d.diamond@gmail.com>
2022-12-04 23:20:49 -0700

    Card Randomization
    Card Randomization working with the rest of it.

--- My project/Assets/CardManager.cs
+++ My project/Assets/CardManager.cs
@@ -13,6 +13,10 @@ public class CardManager : MonoBehaviour
     [SerializeField]
     private CardUpdater []cards;
 
+
+    [SerializeField]
+    private List<int> _order;
+
     [SerializeField]
     private GameObject []locations;
 
@@ -35,15 +39,30 @@ public class CardManager : MonoBehaviour
 
         for(int i=0; i<locations.Length; i++)
         {
+            _order.Add(i);
+        }
+
+        for(int i=0; i<locations.Length; i++)
+        {
             Debug.Log(i);
-            tempCardPrefab = Instantiate(cardPrefab, locations[i].transform);
+            tempCardPrefab = Instantiate(cardPrefab, locations[RandomOrder()].transform);
             tempCard = (CardUpdater)tempCardPrefab.GetComponent(typeof(CardUpdater));
             tempCard.UpdateCardInfo(wm.getEnglishWord(i), i);
             cards[i] = tempCard;
         }
     }
 
-    
+    int RandomOrder()
+    {
+        int i = Random.Range(0, _order.Count);
+        int n = _order[i];
+
+        _order.RemoveAt(i);
+
+        return n;
+    }
+
+
     void AdvanceIndex(int posIndex, int wordIndex)
     {
         Debug.Log("Position index is "+ posIndex + " word index is " + wordIndex);
